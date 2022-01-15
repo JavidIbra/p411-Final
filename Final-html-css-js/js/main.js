2ec128e3e99faab4c5990f2b5f804dba373d80d5
JavidIbra <javidkhi@code.edu.az>
2022-01-14 21:21:28 -0800

    first

---
+++ Final-html-css-js/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  AOS.init();
+
+  updateNav();
+
+  $(window).scroll(function () {
+    updateNav();
+  });
+
+  function updateNav() {
+    if ($(window).scrollTop() > 30) {
+      $(".navbar").addClass("scrolled");
+    } else {
+      $(".navbar").removeClass("scrolled");
+    }
+  }
+
+  $(document).on("click", ".work-button", function () {
+    $(this).addClass("active");
+  });
+});
